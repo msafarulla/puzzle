0e8c6313197008cccc1fa7f6516053b5ceb7d127
Mohamed <msafarulla@users.noreply.github.com>
2020-05-26 00:04:02 -0700

    Updated with assert clause

--- puzzle.py
+++ puzzle.py
@@ -1,14 +1,28 @@
-string=')))()((((ljlkj||||((((()))))))'
-for i in range(1,len(string)+1):
-    where_ob = string.find(i*'(')
-    where_cb = string.find(i*')',where_ob)
-    if where_ob < where_cb:
-        a,b=where_ob+i,where_cb+1,
-    else:
-        print(a, b, i-1)
-        break
+#Assumption: There would be at least one '()' in the string
+strings = {
+                ')((ljlkj||||((((()))))))':5,
+                '((((()))()()((((((':3,
+                '()))())))(((())))))))':4,
+                '))((()))))))':3,
+                '(()))))))(((()':2,
+
+          }
+
+def fun(string):
+    a,b=0,0
+    for i in range(1,len(string)+1):
+        where_ob = string.find(i*'(')
+        where_cb = string.find(i*')',where_ob)
+        if where_ob < where_cb:
+            a,b=where_ob+i,where_cb+1,
+        else:break
+    return (i-1)
 
 
+for i in strings:
+    assert (fun(i) == strings[i]), "Invalid results"
+
+print("PASSED")
 
 
 
